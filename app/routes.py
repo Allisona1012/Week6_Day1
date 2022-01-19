cc76919e41295712a08be88956eb262f5b0f7168
Allisona1012 <allisona1012@gmail.com>
2022-01-18 19:57:57 -0600

    Started HTML

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    my_name = 'allison'
+    return render_template('index.html')
 
 @app.route('/name')
 def name():
