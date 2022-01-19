3b833786cfce8e39956597db6b97882eaed3107d
Allisona1012 <allisona1012@gmail.com>
2022-01-18 21:07:33 -0600

    Forms able to be validated

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,9 @@ def index():
     my_name = 'allison'
     return render_template('index.html')
 
-@app.route('/book')
+@app.route('/book', methods = ["GET","POST"])
 def book():
     form = RegisterForm()
+    if form.validate_on_submit():
+        print('FORM HAS BEEN VALIDATED!')
     return render_template('book.html', form = form)
