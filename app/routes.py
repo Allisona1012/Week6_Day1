b7bc7b82ada653c957b34b2a64cfc5288f502dd6
Allisona1012 <allisona1012@gmail.com>
2022-01-18 21:16:39 -0600

    Redirect working

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,redirect,url_for
 from app.forms import RegisterForm
 
 @app.route('/')
@@ -12,4 +12,10 @@ def book():
     form = RegisterForm()
     if form.validate_on_submit():
         print('FORM HAS BEEN VALIDATED!')
+        full_name= form.full_name.data
+        email = form.email.data
+        phone_number= form.phone_number.data
+        return redirect(url_for('index'))
+
+
     return render_template('book.html', form = form)
